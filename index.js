96dae2c326926279c15c24ad8a9d38832138f3d7
oakdorak <oscarrobledo1793@gmail.com>
2022-08-02 18:52:43 -0500

    intento con JS

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ menuItems.forEach((item, index) => {
             wrapper.style.transform = `translateX(${-100 * index}vw)`;
         });
 });
+const gomas = [ {
+    sabor : "mora",
+    mg : 10,
+    precio : 100,
+    stock : true
+},
+    {
+    sabor : "fresa",
+    mg : 15,
+    precio : 150,
+    stock : true
+}]
+
+console.log(gomas[0])
